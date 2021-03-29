2ce18136927956deb94dc1bbf85604103a2c1b2c
Destany Brown <Georgiafbi@gmail.com>
2021-03-29 17:55:40 -0400

    created ufo table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,57 @@
 // from data.js
-var tableData = data;
-
+var UFOData = data;
+//console.log(UFOData);
 // YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+
+
+// Console.log the alien data from data.js
+console.log(UFOData);
+function UFOTable() {
+    UFOData.forEach((UFOReport) => {
+        var row = tbody.append("tr");
+        //transform the data json object format into a list
+        var entries = Object.entries(UFOReport);
+        //console.log(entries);
+        entries.forEach(([key, value]) => {
+            var cell = row.append('td');
+            console.log(key);
+            console.log(value);
+            cell.text(value);
+        });
+    });
+}
+UFOTable()
+
+//Select the form
+// Create event handlers for clicking the button or pressing the enter key
+//button.on("click", runEnter);
+var form = d3.select("form");
+form.on("submit",runEnter);
+// Create the function to run for both events
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    console.log(inputElement);
+
+    function selectDatetime(sighting) {
+
+        return sighting.datetime === inputElement;
+
+    }
+
+    var UFOSighting = UFOData.filter(selectDatetime)
+
+    console.log(UFOSighting)
+
+
+
+}
+
