3553638b81f8709e02521c21162dd4d6fb193ff8
Destany Brown <Georgiafbi@gmail.com>
2021-04-03 03:39:56 -0400

    added reset buttons to both UFO-Level-1 and UFO-Level-2, added multiple filter options for UFO-Level-2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -18,8 +18,8 @@ function UFOTable(jsonData) {
         //console.log(entries);
         entries.forEach(([key, value]) => {
             var cell = row.append('td');
-            console.log(key);
-            console.log(value);
+            //console.log(key);
+            //console.log(value);
             cell.text(value);
         });
     });
@@ -27,36 +27,69 @@ function UFOTable(jsonData) {
 UFOTable(UFOData)
 
 
-// Select the button
-var button = d3.select("#filter-btn");
+// Select the reset and filter buttons
+var filterButton = d3.select("#filter-btn");
+var resetButton = d3.select("#clear-btn")
 //Select the form
-button.on("click", runEnter);
-var form = d3.select("form");
+// d3.selectAll("form-control").on("submit",runEnter);
 
 // Create event handlers for clicking the button or pressing the enter key
-button.on("click", runEnter);
-form.on("submit",runEnter);
+filterButton.on("click", runEnter);
 // Create the function to run for both events
 function runEnter() {
 
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
+    //selectALL this get input value then append to a list values to equate to in out filter function 
     // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
-    var inputValue = inputElement.property("value");
+    var inputArray = ["#datetime", "#city", "#state", "#country", "#shape"];
+    var inputKey=[];
+    var inputValue = [];
+    inputArray.forEach(id => {
+        //selectALL this get input value then append to a list values to equate to in out filter function 
+        // Select the input element and get the raw HTML node
+        var value = d3.select(id).property("value");
+        if (value != "") {
+            inputValue.push(value);
+            inputKey.push(id.slice(1));
+            console.log(value);
+        }
+
+    });
     console.log(inputValue);
 
-    var selectedData = UFOData.filter(sighting => sighting.datetime === inputValue);
+    var selectedData = 0;
 
-    //return this as new json data to update table 
-    console.log(selectedData);
+    // loops through each inputValue and stores filtered data into selectedData
+    for (var i = 0; i < inputValue.length; i++) {
+        //slices element for data key
+        var key= inputKey[i];
+        //return filted data for first input value
+        if (i === 0) {
+            selectedData = UFOData.filter(sighting => sighting[key] === inputValue[i]);
+        }
+        //updates filtered data for the remain input values entered
+        else if (i > 0) {
+            selectedData = selectedData.filter(sighting => sighting[key] === inputValue[i]);
+        }
+    }
+
+    console.log(selectedData)
+    //updates table with filtered data base on input values entered
     UFOTable(selectedData);
 
 
-
-
 }
+// Create event handlers for clicking the buttons or pressing the enter key
+resetButton.on("click", function () {
+
+    // Preven the page from refreshing
+    d3.event.preventDefault();
+    // restore the table to unfiltered
+    UFOTable(UFOData);
+});
+
 
 
 
